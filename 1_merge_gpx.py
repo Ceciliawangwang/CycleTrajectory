978109a567f098403984e60b951233bf90521061
Meihui Wang <34533625+Ceciliawangwang@users.noreply.github.com>
2024-06-10 17:58:26 +0100

    Add files via upload
    add mege gpx in step1

---
+++ 1_merge_gpx.py
@@ -0,0 +1,55 @@
+import os
+import pandas as pd
+
+from gpx_handler import GPXHandler
+from utils import df_to_gpx
+
+#########final version#########
+
+def read_gpx_files(folder_path):
+    trajectories = {}
+    for filename in os.listdir(folder_path):
+        if filename.endswith('.gpx'):
+            # look for 'GS' or 'GH' in the filename
+            gs_gh_index = filename.find('GS') if 'GS' in filename else filename.find('GH')
+            if gs_gh_index != -1:
+                # extract the id for the trajectory/entire trip
+                identifier = filename[gs_gh_index:gs_gh_index+2]  # 'GS' or 'GH'
+                trajectory_id = filename[-7:-4]  # with final 3 digits
+                full_id = identifier + trajectory_id
+                if full_id not in trajectories:
+                    trajectories[full_id] = []
+                trajectories[full_id].append(filename)
+    return trajectories
+
+
+def merge_gpx_data(files, folder_path):
+    merged_data = None
+    for file in files:
+        file_path = os.path.join(folder_path, file)
+        gpx_hdl = GPXHandler(file_path)
+        data = gpx_hdl.gpx_to_df()
+        merged_data = pd.concat([merged_data, data]) if merged_data is not None else data
+    return merged_data
+
+
+def save_gpx_file(merged_gpx, output_path, participant_id, full_id):
+    with open(os.path.join(output_path, f'{participant_id}_{full_id}.gpx'), 'w') as f:
+        f.write(merged_gpx.to_xml())
+
+
+def process_folder(top_folder, output_path):
+    for root, dirs, files in os.walk(top_folder):
+        participant_id = os.path.basename(root)  # get the participant id
+        trajectories = read_gpx_files(root)
+        for full_id, files in trajectories.items():
+            files.sort(key=lambda x: int(x[x.find('GS') + 2:x.find('GS') + 4]) if 'GS' in x else int(x[x.find('GH') + 2:x.find('GH') + 4]))
+            merged_data = merge_gpx_data(files, root)
+            merged_gpx = df_to_gpx(merged_data)
+            save_gpx_file(merged_gpx, output_path, participant_id, full_id)
+
+
+
+folder_path = '/xxxxxx/data/gpx_filtered/'  # gpx_folder
+output_path = '/xxxxxx/data/gpx_merged/'     # output_folder
+process_folder(folder_path, output_path)
